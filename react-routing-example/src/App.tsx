a77fd2a0501db9a2d26bfc40a690e66761a31cfe
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-31 17:50:16 +0530

    refactored version

--- react-routing-example/src/App.tsx
+++ react-routing-example/src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
 
-import './App.css';
+import SideBar from './RoutingExample/SiderBar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import FirstTeam from './RoutingExample/FirstTeam';
+import SecondTeam from './RoutingExample/SecondTeam';
+import TeamSummary from './RoutingExample/TeamSummary';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path='/' component={SideBar}></Route>
+        </Switch>
+        <Switch>
+          <Route path='/teamSummary' component={TeamSummary}></Route>
+        </Switch>
+        <Switch>
+          <Route path='/firstTeam' component={FirstTeam}></Route>
+        </Switch>
+        <Switch>
+          <Route path='/secondTeam' component={SecondTeam}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ react-routing-example/src/RoutingExample/SecondTeam.tsx
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import 'antd/dist/antd.css';
+import '../index.css';
+import { Layout } from 'antd';
+import SiderBar from './SiderBar';
+
+const { Sider } = Layout;
+export default class secondTeam extends React.Component {
+
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+
+    const { collapsed } = this.state;
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+          <SiderBar />
+        </Sider>
+        <>
+
+          <div id="container" style={{ padding: "44px" }}>
+
+            <h1>Second Group</h1>
+          </div>
+        </>
+
+      </Layout>
+    );
+  }
+}
+
